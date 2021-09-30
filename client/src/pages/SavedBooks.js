9c47be682f0fdc07b1b9794009687fcd969c33fb
Michael Souter <souterm1@gmail.com>
2021-09-29 22:45:49 -0400

    SavedBooks.js refactored

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
 import {
@@ -14,10 +14,8 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  
-
-  
-  
+  const { loading, data } = GET_ME;
+  const [deleteBook, { error }] = REMOVE_BOOK;
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -28,14 +26,9 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
+      await deleteBook({
+        variables: { bookId },
+      });
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -44,8 +37,8 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
+  if (loading) {
+    return <h2>LOADING...PLEASE WAIT....</h2>;
   }
 
   return (
