e57433016f9afea0d4ab054cb2274faff558c5d3
Michael Souter <souterm1@gmail.com>
2021-10-01 23:43:10 -0400

    committing what I have for the sake of TA Help

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -16,7 +16,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [createUser, { error, data }] = useMutation(ADD_USER);
+  const [createUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -34,18 +34,18 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await createUser({
+      const response = await createUser({
         variables: { ...userFormData },
       });
 
-      console.log(data);
-      /* if (!response.ok) {
+      console.log(response);
+      if (!response.ok) {
         throw new Error("OH NO!SOMETHING WENT WRONG!");
-      }*/
+      }
 
-      //const { token, user } = await data.json();
-      //console.log(user);
-      Auth.login(data.createUser.token);
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
